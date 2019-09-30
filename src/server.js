6028bb07d01d561f82baf1f533fd6bc71147961e
Gustavo Sobrinho <gustavo.sobrinho@crmeducacional.com>
2019-09-30 11:06:59 -0300

    Ultima correção para o Heroku

--- src/server.js
+++ src/server.js
@@ -8,7 +8,6 @@ const server = require('http').Server(app);
 const io = require('socket.io') (server);
 mongoose.set('useUnifiedTopology', true);
 
-
 app.use(cors());
 
 io.on('connection', socket => {
@@ -25,18 +24,17 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-caihv.mongodb.net/o
 
 app.use((req, res, next) => {
     req.io = io; // Informação global para aplicação
-
     return next(); // Sem o next, as requisições iriam parar aqui.
 });
 
-app.get ('/', (req, res) => res.send('To rondando na raiz,  Pai!'));
+ app.get ('/', (req, res) => res.send('To rondando na raiz, manda a rota!'));
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Permite envio de arquivos
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
-app.use('/routes', routes);
+app.use('/', routes);
 
 // app.listen(3333);    
 server.listen(process.env.PORT || 3333); // A Aplicação já houve protocolo http e websocket
